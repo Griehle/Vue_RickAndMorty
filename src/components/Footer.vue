66c2a43b1fda8bfb49ddfbd17b6979957ddc7a8d
Gary Riehle <gary.riehle@nef1.org>
2020-03-11 14:12:14 -0600

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>
+            <b>2020</b> - Built with Vue <span>❤</span>
+            <b> by Griehle</b>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    p, a {
+        color: #2bee50;
+        margin: 30px 0 30px 0;
+        font-size: 14px;
+    }
+    span{
+        color:red;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h1>My Api project in Vue</h1>
+        <p>Wanna see <b>new characters!</b></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "header"
+    }
+</script>
+
+<style scoped>
+    .container {
+        /* display: flex;
+       flex-direction: column; */
+        align-items: center;
+    }
+    h1 {
+        background: -webkit-linear-gradient(#FFF720, #3CD500);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        font-family: 'Bigelow Rules', cursive; /* Font link: <link href="https://fonts.googleapis.com/css?family=Bigelow+Rules&display=swap" rel="stylesheet"> */
+        font-size: 5em;
+        margin-top: 30px;
+        margin-bottom: 0px;
+    }
+    p {
+        color: #12ee6d;
+        margin-bottom: 30px;
+    }
+</style>
